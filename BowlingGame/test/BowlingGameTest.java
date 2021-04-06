e56347ff2b64a009390b19f726496d9d1b6ecc6a
Rhcsky <dev.rhcsky@gmail.com>
2021-04-06 15:06:29 +0900

    Add testAllOnes

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
     @Test
-    public void testGutterGame(){
+    public void testGutterGame() {
         Game game = new Game();
         for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
-        assertEquals(0,game.score());
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20,game.score());
     }
 }
